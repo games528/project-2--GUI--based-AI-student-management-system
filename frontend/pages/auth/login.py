07caa3ee85f792600aabcdb68a58ea01188a0d11
samridh tiwari <samridhtiwari6@gmail.com>
2026-08-13 20:04:35 +0530

    login page completed

--- frontend/pages/auth/login.py
+++ frontend/pages/auth/login.py
@@ -5,6 +5,28 @@ def toggle_password():
         password_entry.configure(show = "")
     else:
         password_entry.configure(show = "*")
+
+def authenticate_user(username,password):
+    if username == "admin" and password == "1234":
+        return True
+    return False        
+
+def validate_login():
+    username = username_entry.get().strip()
+    password = password_entry.get()
+    if not username:
+        error_label.configure(text = "please enter your email/username")
+        return
+    if not password:
+        error_label.configure(text = "please enter your password")
+        return
+    if authenticate_user(username,password):
+        error_label.configure(text = "Login successful")
+    else:
+        error_label.configure(text = "invalid username or password")
+
+
+
 app = ctk.CTk()
 
 app.title("AI student management system")
@@ -27,5 +49,14 @@ password_entry = ctk.CTkEntry(password_frame, width= 300, height= 40, placeholde
 password_entry.pack(side = "left")
 show_password_button = ctk.CTkButton(password_frame, text = "(.)", width= 40, height = 40, command = toggle_password)
 show_password_button.pack(side = "left", padx = (5,0))
+
+remember_me = ctk.CTkCheckBox(main_frame, text = "Remember me")
+remember_me.pack(anchor = "w", padx =75, pady = (5, 15))
+
+error_label = ctk.CTkLabel(main_frame,text = "",text_color = "red", font = ctk.CTkFont(size = 14))
+error_label.pack(pady = (0,5))
+
+login_button = ctk.CTkButton(main_frame, text = "Login", width = 300, height = 40, command = validate_login)
+login_button.pack(pady = (10, 20))
 
 app.mainloop()
